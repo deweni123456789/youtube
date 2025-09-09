06de5d49671b65ab142138df3b9932ed946f7939
deweni123456789 <schoford91@gmail.com>
2025-09-09 11:16:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 from modules.song import song_handler
 from modules.video import video_handler
 
-API_ID = int(os.getenv("API_ID", "12345"))       # Put your values
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "5047271"))       # Put your values
+API_HASH = os.getenv("API_HASH", "047d9ed308172e637d4265e1d9ef0c27")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7896090354:AAE_NaVu_d-x-TCJt9CPNMl9t94Mltw_jrw")
 
 app = Client(
     "pytube_bot",
